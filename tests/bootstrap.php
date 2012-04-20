59258e8a645176ae89a95c2a36ea54283490dc67
Nicolas Le Goff <legoff.n@gmail.com>
2012-04-20 18:05:47 +0200

    modify bootstrap for testes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,5 @@
 <?php
 
-
-ini_set("display_errors", "on");
 require_once __DIR__ . '/../vendor/.composer/autoload.php';
-require_once __DIR__ . '/../vendor/alchemy/phraseanet-clients/PHP/Client.php';
 
 
