12105559e2562bbac0d089163bb05ea04ae274fb
Romain Neutron <imprec@gmail.com>
2012-06-14 10:13:16 +0200

    Add abstract class

---
+++ tests/Test/Repository/Repository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Test\Repository;
+
+use PhraseanetSDK\Client;
+use Guzzle\Http\Plugin\MockPlugin;
+use Guzzle\Http\Message\Response;
+use Guzzle\Http\Client as GuzzleClient;
+use Monolog\Logger;
+use Monolog\Handler\NullHandler;
+
+abstract class Repository extends \PHPUnit_Framework_TestCase
+{
+
+    protected function getClient($response)
+    {
+        $plugin = new MockPlugin();
+        $plugin->addResponse(new Response(
+                200
+                , null
+                , $response
+            )
+        );
+
+        $clientHttp = new GuzzleClient(
+                'http://my.domain.tld/api/v{{version}}',
+                array('version' => 1)
+        );
+        $clientHttp->getEventDispatcher()->addSubscriber($plugin);
+
+        $logger = new Logger('tests');
+        $logger->pushHandler(new NullHandler());
+
+        return new Client('http://my.domain.tld/', '123456', '654321', $clientHttp, $logger);
+    }
+}
