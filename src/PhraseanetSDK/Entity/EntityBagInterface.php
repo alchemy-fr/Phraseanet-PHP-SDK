8c361dd475a852a03dd4ba6378beeb5b8500994d
Nicolas Le Goff <legoff.n@gmail.com>
2012-12-20 13:22:19 +0100

    Update EntityBagInterface

--- src/PhraseanetSDK/Entity/EntityBagInterface.php
+++ src/PhraseanetSDK/Entity/EntityBagInterface.php
@@ -4,9 +4,5 @@ namespace PhraseanetSDK\Entity;
 
 class EntityBagInterface implements EntityInterface
 {
-    public function set($key, $value);
     
-    public function get($key);
-    
-    public function getBag();
 }
