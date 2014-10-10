ac633297a1060ff08fb123ea5eeec83ac05ac418
Nicolas Le Goff <legoff.n@gmail.com>
2014-10-10 09:52:25 +0200

    Update version

--- src/PhraseanetSDK/ApplicationInterface.php
+++ src/PhraseanetSDK/ApplicationInterface.php
@@ -20,7 +20,7 @@ interface ApplicationInterface
     const USER_AGENT = 'Phraseanet SDK';
 
     /** @var string SDK version */
-    const VERSION = '0.5';
+    const VERSION = '0.6.x-dev';
 
     /**
      * Return an OAuth2Connector
