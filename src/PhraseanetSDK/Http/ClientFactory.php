a05989ff9b56fa16686c0bfdb53aad3ecd94eeff
Thibaud Fabre <thibaud@aztech.io>
2016-08-05 19:41:53 +0200

    WIP

--- src/PhraseanetSDK/Http/ClientFactory.php
+++ src/PhraseanetSDK/Http/ClientFactory.php
@@ -11,7 +11,39 @@
 
 namespace PhraseanetSDK\Http;
 
-class ClientFactory 
+use GuzzleHttp\Client;
+
+class ClientFactory
 {
 
+    public function createClient()
+    {
+        if (interface_exists('GuzzleHttp\ClientInterface')) {
+            return $this->createGuzzleClient();
+        }
+
+        if (interface_exists('Guzzle\Http\ClientInterface')) {
+            return $this->createLegacyGuzzleClient();
+        }
+
+        throw new \RuntimeException('No HTTP adapter is available.');
+    }
+
+    /**
+     * @return \GuzzleHttp\ClientInterface|Client
+     */
+    private function createGuzzleClient()
+    {
+        $client = new Client([]);
+
+        return new
+    }
+
+    /**
+     * @return \Guzzle\Http\Client|Client
+     */
+    private function createLegacyGuzzleClient()
+    {
+
+    }
 }
---
+++ src/PhraseanetSDK/Http/GuzzleHttp/GuzzleHttpClient.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of Phraseanet-PHP-SDK.
+ *
+ * (c) Alchemy <info@alchemy.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PhraseanetSDK\Http\GuzzleHttp;
+
+use PhraseanetSDK\Http\Client;
+
+class GuzzleHttpClient implements Client
+{
+
+    /**
+     * @param string $method
+     * @param string $path
+     * @param array $query
+     * @param array $postFields
+     * @param array $files
+     * @param array $headers
+     * @return string
+     */
+    public function call(
+        $method,
+        $path,
+        array $query = array(),
+        array $postFields = array(),
+        array $files = array(),
+        array $headers = array()
+    ) {
+        // TODO: Implement call() method.
+    }
+}
